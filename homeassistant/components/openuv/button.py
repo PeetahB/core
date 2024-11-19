888b8543bbc45b286b75daa634bca35bf523a696
PeetahB <petrasuper@gmail.com>
2024-11-19 19:35:26 +0100

    Feature: Add a field for sun exposure time and basic_name

--- homeassistant/components/openuv/button.py
+++ homeassistant/components/openuv/button.py
@@ -8,6 +8,10 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
+from .const import DATA_UV, DOMAIN
+from .coordinator import OpenUvCoordinator
+from .sensor import get_uv_label
+
 
 @dataclass(frozen=True, kw_only=True)
 class OpenUVButtonEntityDescription(ButtonEntityDescription):
@@ -20,7 +24,7 @@ ENTITY_DESCRIPTIONS = [
     OpenUVButtonEntityDescription(
         key="vitamin_d",
         name="Vitamin D",
-        press_action=lambda x: get_smt(x),  # pylint: disable=unnecessary-lambda
+        press_action=lambda coordinator, button: get_smt(coordinator, button),  # pylint: disable=unnecessary-lambda
         icon="mdi:target",
     ),
     # OpenUVButtonEntityDescription(
@@ -38,19 +42,32 @@ async def async_setup_entry(
     """Set up the openuv buttons."""
     # Create and add the switch entity
     _ = hass, entry
-    async_add_entities(OpenUVButton(description) for description in ENTITY_DESCRIPTIONS)
+    coordinators: dict[str, OpenUvCoordinator] = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities(
+        OpenUVButton(coordinators[DATA_UV], description)
+        for description in ENTITY_DESCRIPTIONS
+    )
 
 
 class OpenUVButton(ButtonEntity):
     """Representation of openuv button."""
 
     entity_description: OpenUVButtonEntityDescription
+    sun_exposure_time: int
+    basic_name: str
 
-    def __init__(self, entity_description: OpenUVButtonEntityDescription) -> None:
+    def __init__(
+        self,
+        coordinator: OpenUvCoordinator,
+        entity_description: OpenUVButtonEntityDescription,
+    ) -> None:
         "Initialize the button."
         super().__init__()
         self.entity_description = entity_description
         self._key = entity_description.key
+        self._coordinator = coordinator
+        self.sun_exposure_time = 0
+        self.basic_name = "Vitamin D button"
 
     @property
     def unique_id(self) -> str:
@@ -59,10 +76,21 @@ class OpenUVButton(ButtonEntity):
 
     async def async_press(self) -> None:
         """Handle the button press."""
-        self.entity_description.press_action(self)
+        self.entity_description.press_action(self._coordinator, self)
+        # self.async_write_ha_state()
 
 
-def get_smt(button_entity_description: OpenUVButtonEntityDescription) -> None:
+def get_smt(coordinator: OpenUvCoordinator, button_entity: OpenUVButton) -> None:
     "Extend the name of the button (treated as description)."
-    if isinstance(button_entity_description.name, str):
-        button_entity_description.name = button_entity_description.name + " 20mins"
+    # print(f"name before calling: {button_entity.entity_description.name}")
+    if "uv" in coordinator.data:
+        # I need to know the skin type in order to calculate the vitamin D intake
+        uv_level = get_uv_label(coordinator.data["uv"])  # gets the current uv index
+        _ = uv_level
+        # print(f"current uv level: {uv_level}")
+    if isinstance(button_entity.name, str):
+        button_entity.sun_exposure_time = 20  # so far hardcoded
+        button_entity.name = (
+            button_entity.basic_name + str(button_entity.sun_exposure_time) + " mins"
+        )
+        # print(f"after calling: {button_entity.entity_description.name}")
